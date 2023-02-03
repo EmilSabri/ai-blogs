80fa1426307b0f943e0b1f979d56608efe81fd1a
Emil Sabri <emil.sabri@gmail.com>
2023-02-02 18:30:04 -0800

    Added ability to retrieve article

---
+++ src/routes/[article_slug]/+page.server.js
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import { S3_BUCKET_ARTICLES } from "$env/static/private";
+import { s3Client } from "$lib/server";
+
+
+
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+    const key = `${params.article_slug}`
+
+    const markdown = await s3Client.getObject(S3_BUCKET_ARTICLES, `${key}/markdown.md`)
+    const metadata = await s3Client.getObject(S3_BUCKET_ARTICLES, `${key}/metadata.json`)
+    const metaJson = JSON.parse(metadata)
+
+  return {
+        markdown: markdown,
+        metadata: metaJson
+    };
+}
---
+++ src/routes/[article_slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let data;
+	let markdown = data.markdown;
+	let metadata = data.metadata;
+</script>
+
+<h1>
+	{metadata.title}
+</h1>
