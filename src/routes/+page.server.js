d19e30664ede7a76db818a55613ba1cb41f1271a
Emil Sabri <emil.sabri@gmail.com>
2023-02-13 14:52:52 -0800

    Set articles to private

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,7 +4,7 @@ import { articles } from "$lib/server/articles"
 export async function load() {
 
     // Todo - Get list of articles from redis
-    const articlesMeta = await articles.getPrivateArticles(true, 10)
+    const articlesMeta = await articles.getPublicArticles(true, 10)
     
     return {
         articles: articlesMeta,
